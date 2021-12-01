3d22f03e8532ba72f731aeea5e0f21df5796345b
Dmytro <dmytro27kagirov@gmail.com>
2021-12-01 19:25:01 +0200

    translation added

--- src/screens/Post.js
+++ src/screens/Post.js
@@ -74,7 +74,7 @@ function Post({navigation}){
             <TouchableOpacity
                 style={styles.panelButton}
                 onPress={() => bs.current.snapTo(1)}>
-                <Text style={styles.panelButtonTitle}>Cancel</Text>
+                <Text style={styles.panelButtonTitle}>{t('post:cancel_photo')}</Text>
             </TouchableOpacity>
         </View>
     );
