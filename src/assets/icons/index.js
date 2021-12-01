61be278c782e1754859c5f75aaf997ddfae0cf50
Oleksandr Kashytskyi <oleksandr.kashytskyi@student.tuke.sk>
2021-12-01 20:36:59 +0100

    Google icon added

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,5 @@
 const profile = require('./img/profile.png')
-const google = require('./img/google.jpg')
+const google = require('./img/google.png')
 const filters = require('./img/filters.png')
 
 
